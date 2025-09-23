d839ca1536f9527345594dd5346c3f73afa21ce7
hassan9khaled <hassan9khaled@gmail.com>
2025-09-23 14:31:34 +0300

    define the enums

---
+++ backend/app/core/config.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+
+    """Application settings loaded from environment variables."""
+    
+    APP_NAME: str
+    APP_VERSION: str
+
+    FILE_MAX_SIZE: int
+    FILE_ALLOWED_TYPES: list
+
+    class Config:
+        env_file = "./app/.env"
+
+
+def get_settings():
+    return Settings()
