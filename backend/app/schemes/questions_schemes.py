39ad5ddd6d234318c5a92b5cafdc2bc3144409e8
hassan9khaled <hassan9khaled@gmail.com>
2025-10-02 00:27:19 +0300

    style(questions): add answers to the question response

--- backend/app/schemes/questions_schemes.py
+++ backend/app/schemes/questions_schemes.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel  
-
+from typing import Optional
+from .answers_schemes import AnswerOut
 
 class QuestionOut(BaseModel):
     id: int
     text: str
+    answer: Optional[AnswerOut] = None
     order: int
 
     class Config:
